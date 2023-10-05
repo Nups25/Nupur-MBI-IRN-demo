3001f1797067ff396ca8d2863e44f132f68087c7
Nups25 <142568102+Nups25@users.noreply.github.com>
2023-10-05 17:19:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,72 +1,14 @@
-# Configure the Azure provider
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
+ backend "azurerm" {
+   resource_group_name = "mbirn_demo"
+   storage_account_name = "mbirnsa"
+   container_name = "mbirndemo"
+   key = "dev.terraform.tfstate"
+ }
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "demo" {
-  name     = "demo-resources"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "demo" {
-  name                = "demo-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
-}
-
-resource "azurerm_subnet" "demo" {
-  name                 = "demo-subnet"
-  resource_group_name  = azurerm_resource_group.demo.name
-  virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "demo" {
-  name                = "demo-nic"
-  location            = azurerm_resource_group.demo.location
-  resource_group_name = azurerm_resource_group.demo.name
-
-  ip_configuration {
-    name                          = "demo-config"
-    subnet_id                     = azurerm_subnet.demo.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "demo" {
-  name                  = "demo-machine"
-  resource_group_name   = azurerm_resource_group.demo.name
-  location              = azurerm_resource_group.demo.location
-  size                  = "Standard_DS1_v2"
-  admin_username        = "your_admin_username"
-  network_interface_ids = [azurerm_network_interface.demo.id]
-
-  admin_ssh_key {
-    username   = "your_admin_username"
-    public_key = file("~/.ssh/id_rsa.pub") # Replace with your public key path
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+module "RG" {
+  source = "./modules/RG"
+  rgname = var.rgname
+  location = var.location
 }
