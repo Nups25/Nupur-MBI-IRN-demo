5b7f621d76cb41a28801c49e5b32703569a246ad
Nups25 <142568102+Nups25@users.noreply.github.com>
2023-10-05 17:24:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rgname" {
+   desrciption = "Resource Group Name"
+   default     = "mbirn_demo"
+   type        = string 
+}
+
+variable "location" {
+   desrciption = "Azure Location"
+   default     = "US EAST"
+   type        = string 
+}
